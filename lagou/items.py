6b3dfae16be4397544b8e2f39bca3a4b9dc053f3
ggqshr <942490944@qq.com>
2019-10-22 16:58:42 +0800

    初步完成没有检查细节

--- lagou/items.py
+++ lagou/items.py
@@ -6,9 +6,63 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import SelectJmes, Compose, MapCompose
+import json
+
+
+class LagouItemLoader(ItemLoader):
+    pass
 
 
 class LagouItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    link = scrapy.Field()  # url
+    id = scrapy.Field()  # rloc
+    post_time = scrapy.Field(
+        input_processor=SelectJmes("createTime"),
+    )  # lastmod
+    job_name = scrapy.Field(
+        input_processor=SelectJmes("positionName"),
+    )  # title
+    salary = scrapy.Field(
+        input_processor=SelectJmes("salary"),
+    )  # salary
+    place = scrapy.Field(
+        input_processor=SelectJmes("city"),
+    )  # city
+    job_nature = scrapy.Field(
+        input_processor=SelectJmes("jobNature"),
+    )  # type
+    experience = scrapy.Field(
+        input_processor=SelectJmes("workYear"),
+    )  # experience
+    education = scrapy.Field(
+        input_processor=SelectJmes("education"),
+    )  # education
+    # job_number = scrapy.Field()  # number
+    job_kind = scrapy.Field(
+        input_processor=SelectJmes("firstType"),
+    )  # jobsecondclass
+    advantage = scrapy.Field(
+        input_processor=SelectJmes("positionAdvantage"),
+    )  # ori_welfare
+    company_name = scrapy.Field(
+        input_processor=SelectJmes("companyFullName"),
+    )  # officialname
+    company_size = scrapy.Field(
+        input_processor=SelectJmes("companySize"),
+    )  # size
+    company_industry = scrapy.Field(
+        input_processor=SelectJmes("industryField"),
+    )  # industry
+    company_address = scrapy.Field()  # companyaddress
+    company_nature = scrapy.Field()  # employertype
+    job_content = scrapy.Field()
+    job_place = scrapy.Field()
+    # company_homepage = scrapy.Field()  # official
+    hot_score = scrapy.Field()  # 百度对于本条招聘信息的热度评分  hot_score
+    job_safety_score = scrapy.Field()  # 百度对于招聘信息的安全评分  job_safety_score
+    company_reputation_score = scrapy.Field()  # 百度对于招聘公司的声誉评分 company_reputation_score
+    salary_level_score = scrapy.Field()  # 对于薪水评分  salary_level_score
--- lagou/spiders/lg.py
+++ lagou/spiders/lg.py
@@ -1,44 +1,173 @@
 # -*- coding: utf-8 -*-
+import uuid
+
 import scrapy
 from scrapy import Request, FormRequest
 from scrapy.http import Response
 from scrapy.http.cookies import CookieJar
+from urllib.parse import quote
 
 
 class LgSpider(scrapy.Spider):
     name = 'lg'
     allowed_domains = ['lagou.com']
+    fill_city_url = "https://www.lagou.com/jobs/list_/p-city_{city}?px=new&gx=&isSchoolJob=1#filterBox"
+    fill_district_url = "https://www.lagou.com/jobs/list_/p-city_{city}?px=new&gx=&isSchoolJob=1&district={district}}#filterBox"
+    fill_bizArea_url = "https://www.lagou.com/jobs/list_/p-city_{city}?px=new&district={district}&bizArea={bizArea}#filterBox"
     origin_url = "https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false"
     header_dict = {
         'Referer': 'https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput=',
         'Accept': 'application/json, text/javascript, */*; q=0.01',
-        # "Accept-Encoding": "gzip, deflate, br",
-        # "Accept-Language": "zh-CN,zh;q=0.9",
-        # "Cache-Control": "max-age=0",
-        # "Connection": "keep-alive",
-        # "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        # "Origin": "https://www.lagou.com",
-        "X-Requested-With":"XMLHttpRequest",
+        "X-Requested-With": "XMLHttpRequest",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
     }
-    cookie_jar = CookieJar()
+
+    def __init__(self, city_list, **kwargs):
+        super().__init__(**kwargs)
+        self.city_info = city_list
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = cls(city_list=crawler.settings.get("CITY_INFO"))
+        return spider
 
     def start_requests(self):
         get_cookies_url = "https://www.lagou.com/jobs/list_?labelWords=&fromSearch=true&suginput="
-        yield Request(url=get_cookies_url, callback=self.pass_cookies_to_post, meta={"cookiejar": "lagou"},
-                      headers=self.header_dict)
+        yield Request(url=get_cookies_url, callback=self.parse_by_city, meta={"cookiejar": "lagou"},
+                      headers=self.header_dict, priority=4)
 
-    def pass_cookies_to_post(self, response: Response):
-        self.cookie_jar.extract_cookies(response, response.request)
-        this_req = FormRequest(
-            url=self.origin_url,
-            formdata={"first": "true", "pn": "1",'kd':""},
-            callback=self.parse,
-            meta={"cookiejar": "lagou"},
-            method="POST"
-        )
-        yield this_req
+    def parse_by_city(self, response):
+        """
+        按照城市进行遍历
+        :param response:
+        :return:
+        """
+        num = response.xpath("//a[@id='tab_pos']/span/text()").extract_first()
+        if num == '500+':
+            # 遍历所有的城市
+            for city, code in self.city_info.items():
+                yield Request(
+                    url=self.fill_city_url.format(city=code),
+                    meta={"city": city, "city_code": code, "cookiejar": "lagou"},
+                    headers=self.header_dict,
+                    callback=self.parse_by_district
+                )
+        else:
+            # 直接抓取
+            for pageNum in range(1, 31):
+                yield FormRequest(
+                    url=self.origin_url,
+                    formdata={"first": "true", "pn": pageNum, 'kd': ""},
+                    callback=self.parse,
+                    meta={"cookiejar": "lagou"},
+                    method="POST",
+                    headers=self.header_dict,
+                    priority=3
+                )
+
+    def parse_by_district(self, response):
+        """
+        按照城市划分还是太多，按照城市的区域划分
+        :param response:
+        :return:
+        """
+        num = response.xpath("//a[@id='tab_pos']/span/text()").extract_first()
+        this_city = response.meta['city']
+        this_city_code = response.meta['city_code']
+        if num == '500+':
+            # 遍历该城市所有的区
+            all_district: list = response.xpath("//div[@data-toggle-type='district']//a/text()").extract()
+            if len(all_district) == 0:
+                all_district = response.xpath('//li[@data-toggle-type="district"]//a/text()').extract()
+            all_district.remove("不限")
+            for district in all_district:
+                yield Request(
+                    url=self.fill_district_url.format(city=this_city_code, district=district),
+                    meta={"district": district, "city": this_city, "city_code": this_city_code, "cookiejar": "lagou"},
+                    headers=self.header_dict,
+                    callback=self.parse_by_bizArea
+                )
+        else:
+            # 直接抓取
+            total_page_num = response.xpath("//span[@class='span totalNum']/text()").extract_first()
+            if not total_page_num:
+                pass
+            else:
+                for pageNum in range(1, int(total_page_num) + 1):
+                    yield FormRequest(
+                        url=self.origin_url + f"&city={quote(this_city)}",
+                        formdata={"first": "true", "pn": pageNum, 'kd': ""},
+                        callback=self.parse,
+                        meta={"cookiejar": "lagou"},
+                        method="POST",
+                        headers=self.header_dict,
+                        priority=3
+                    )
+
+    def parse_by_bizArea(self, response):
+        """
+        如果分区的结果还是很多，就按照商圈进行分类
+        :param response:
+        :return:
+        """
+        num = response.xpath("//a[@id='tab_pos']/span/text()").extract_first()
+        this_city = response.meta['city']
+        this_city_code = response.meta['city_code']
+        this_district = response.meta['district']
+        if num == '500+':
+            # 遍历该区的所有商圈
+            all_bizArea: list = response.xpath("//li[@data-toggle-type='bizArea']/div//a/text()").extract()
+            all_bizArea.remove("不限")
+            for bizArea in all_bizArea:
+                yield Request(
+                    url=self.fill_bizArea_url.format(city=this_city_code, district=this_district, bizArea=bizArea),
+                    meta={"bizArea": bizArea, "district": this_district, "city": this_city, "city_code": this_city_code,
+                          "cookiejar": "lagou"},
+                    headers=self.header_dict,
+                    callback=self.parse_bizArea
+                )
+        else:
+            # 直接抓取
+            total_page_num = response.xpath("//span[@class='span totalNum']/text()").extract_first()
+            if not total_page_num:
+                pass
+            else:
+                for pageNum in range(1, int(total_page_num) + 1):
+                    yield FormRequest(
+                        url=self.origin_url + f"&city={quote(this_city)}&district={this_district}",
+                        formdata={"first": "true", "pn": pageNum, 'kd': ""},
+                        callback=self.parse,
+                        meta={"cookiejar": "lagou"},
+                        method="POST",
+                        headers=self.header_dict,
+                        priority=3
+                    )
+
+    def parse_bizArea(self, response):
+        this_city = response.meta['city']
+        this_city_code = response.meta['city_code']
+        this_district = response.meta['district']
+        this_biz_area = response.meta['bizArea']
+        total_page_num = response.xpath("//span[@class='span totalNum']/text()").extract_first()
+        if not total_page_num:
+            pass
+        else:
+            for pageNum in range(1, int(total_page_num) + 1):
+                yield FormRequest(
+                    url=self.origin_url + f"&city={quote(this_city)}&district={this_district}&bizArea={this_biz_area}",
+                    formdata={"first": "true", "pn": pageNum, 'kd': ""},
+                    callback=self.parse,
+                    meta={"cookiejar": "lagou"},
+                    method="POST",
+                    headers=self.header_dict,
+                    priority=3
+                )
 
     def parse(self, response):
+        """
+        解析并填充item
+        :param response:
+        :return:
+        """
         a = response
         yield a
